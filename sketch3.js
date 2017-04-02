7f483501c9a84a398e1ca785a19b1e1e9aca56e9
mikaMek <michael.a.mekonnen@gmail.com>
2017-04-02 21:29:35 +0200

    Camera changes

--- sketch3.js
+++ sketch3.js
@@ -1,9 +1,29 @@
+var xInit;
+var yInit;
+var moveCamY =0;
+var moveCamX =0;
+var moveCamZ =0;
+var lookUpDown=0;
+var lookLeftRight=0;
+var startLooking= false;
+
 
 function setup() {
-	createCanvas(1000,1000, WEBGL);
+	createCanvas(1000,800, WEBGL);
+}
+
+function myInits(event){
+	xInit = event.clientX;
+    yInit = event.clientY;
+    startLooking = true;
 }
 
 function draw() {
+
+	rotateY(radians(lookLeftRight));
+	rotateX(radians(lookUpDown));
+	camera(moveCamX, moveCamY, moveCamZ);
+
 
 	background(250);
 	translate(-100, -100, -300);
@@ -16,4 +36,63 @@ function draw() {
 	box(50,50,50);
 
 
+}
+
+// rotate the camera 
+document.onkeydown = checkKey;
+	function checkKey(e) {
+
+	    e = e || window.event;
+
+	    if (e.keyCode == '38') {
+	        // up arrow
+	        moveCamY-=100;
+	    }
+	    else if (e.keyCode == '40') {
+	        // down arrow
+	        moveCamY+=100;
+	    }
+	    else if (e.keyCode == '37') {
+	       // left arrow
+	       moveCamX-=100;
+	    }
+	    else if (e.keyCode == '39') {
+	       // right arrow
+	       moveCamX+=100;
+	    }
+
+	}
+
+function mouseWheel(event) {
+  print(event.delta);
+  //move the square according to the vertical scroll amount
+ 	moveCamZ -= event.delta;
+ 	document.getElementById('hello').innerHTML = "x: " + moveCamZ ;
+  //uncomment to block page scrolling
+  //return false;
+}
+
+function mouseDragged(){
+	upDown(mouseX, mouseY);
+	leftRight(mouseX, mouseY);
+}
+
+function upDown(mX, mY){
+	if(mX > 0 && mX < 1200 && mY > 0 && mY < 800 && startLooking == true){
+		if(xInit != mX){
+			lookLeftRight += (mX-xInit);
+			xInit=mX;
+		}
+	}
+	return lookLeftRight;
+}
+
+function leftRight(mY, mX){
+	if(mX > 0 && mX < 1200 && mY > 0 && mY < 800 && startLooking == true){
+		if(yInit != mY){
+			lookUpDown += (mY-yInit);
+			yInit=mY;
+		}
+	}
+	return lookUpDown;
 }
